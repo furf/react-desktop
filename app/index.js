a54473a5ebb03349ab6491deceb564f425aac7c3
furf <furf@furf.com>
2015-09-04 11:03:22 -0700

    export an init function from the app module, move configuration to index.html, cleanup

--- app/index.js
+++ app/index.js
@@ -1,51 +1,52 @@
-import React from 'react';
-
 import {combineReducers, createStore} from 'redux';
-import {Provider} from 'react-redux';
-
-import File, {addFile, removeFile} from './models/file';
-import App from './containers/App';
 import Watcher from './services/watcher'
+import path  from 'path';
+import File, {addFile, removeFile} from './models/file';
+import React from 'react';
+import {Provider} from 'react-redux';
+import App from './containers/App';
 
-let store = createStore(combineReducers({
-  images: File
-}));
+/**
+ * Initialize the application.
+ * @param  {String} selector CSS selector for the application's root element.
+ */
+export default function(selector) {
 
-let mainElement = document.querySelector('main');
-
-
-
-const path = require('path');
-const cwd = process.cwd();
-const DATA_DIR = `${cwd}/data`;
-const IMAGES_DIR = `${DATA_DIR}/images`;
-const watcher = new Watcher(IMAGES_DIR);
-
-watcher.on('all', function(event, file) {
-
+  const cwd = process.cwd();
+  const DATA_DIR = `${cwd}/data`;
+  const IMAGES_DIR = `${DATA_DIR}/images`;
   const ADD_EVENT = 'add';
   const UNLINK_EVENT = 'unlink';
-  const relativePath = path.relative(cwd, file);
 
-  if (!relativePath) {
-    return;
-  }
+  // Initialize the data store.
+  const store = createStore(combineReducers({
+    images: File
+  }));
 
-  switch (event) {
-    case ADD_EVENT:
-      store.dispatch(addFile(relativePath));
-    break;
-    case UNLINK_EVENT:
-      store.dispatch(removeFile(relativePath));
-    break;
-  }
-});
+  // Initialize the filesystem watcher.
+  new Watcher(IMAGES_DIR).on('all', function(event, file) {
 
+    const relativePath = path.relative(cwd, file);
 
-React.render(
-  <Provider store={store}>
-    {() => <App />}
-  </Provider>,
-  mainElement
-);
+    if (!relativePath) {
+      return;
+    }
+
+    switch (event) {
+      case ADD_EVENT:
+        store.dispatch(addFile(relativePath));
+      break;
+      case UNLINK_EVENT:
+        store.dispatch(removeFile(relativePath));
+      break;
+    }
+  });
+
+  // Initialize the view.
+  React.render(
+    <Provider store={store}>{() => <App />}</Provider>,
+    document.querySelector(selector)
+  );
+};
+
 
